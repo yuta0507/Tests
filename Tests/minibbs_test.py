af2551279c387c5acd9cd005a73b32154913b41b
吉川祐大 <yuta@yoshikawayuudainoMacBook-Pro.local>
2021-03-14 01:18:40 +0900

    headless実行機能追加

---
+++ Tests/minibbs_test.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+
+driver_path = "/Users/yuta/Chromedriver/chromedriver"
+options = Options()
+options.add_argument('--headless')
+driver = webdriver.Chrome(executable_path=driver_path, options=options)
+
+# 開く
+driver.get("http://localhost:8888/mini_bbs/login.php")
+
+# Email/PASSを入力
+email = driver.find_element_by_name("email")
+email.send_keys("yuta@yuta.com")
+password = driver.find_element_by_name("password")
+password.send_keys("1111")
+
+# ログインボタンをクリック
+login_button = driver.find_element_by_id("login")
+login_button.click()
+
+time.sleep(1)
+
+# メッセージ入力
+input_text = driver.find_element_by_name("message")
+input_text.send_keys("Test posting is succeeded")
+
+# メッセージ投稿
+post = driver.find_element_by_id("post")
+post.click()
+
+time.sleep(1)
+
+# ログアウト
+logout = driver.find_element_by_id("logout")
+logout.click()
+
+time.sleep(1)
+
+# 閉じる
+driver.quit()
